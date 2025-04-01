1b0afeb1e59eefa438831219d3e41168ef084a4e
testmango-sudo <mangotest1965@yahoo.com>
2025-04-01 15:30:52 +0100

    Added display of article comments with article selected for focus

--- src/components/ArticleListBox.jsx
+++ src/components/ArticleListBox.jsx
@@ -1,10 +1,22 @@
-import { React, Route } from "react"
+import { React, Route, useEffect, useState } from "react"
 import { Link, useLocation } from "react-router"
+import { getCommentsByArticleId } from "../api";
 
 
 const ArticleListBox = () => {
     const location = useLocation();
     const data = location.state;
+    const [commentData, setCommentData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => { 
+        getCommentsByArticleId(data.article_id).then((data) => { 
+            console.log(data.comments)
+            setCommentData(data.comments)
+            console.log(commentData)
+            setIsLoading(false)
+        })
+    }, [])
     
     return <div className="single-list-article">
         <ul>
@@ -14,11 +26,21 @@ const ArticleListBox = () => {
             <p>Posted: {data.created_at}</p>
             <p>Votes: {data.votes} 👍</p>
             <img src={data.article_img_url} />
+            <section className="comments">
+                {isLoading ? <div><img src="./src/images/loading.gif" alt="loading comments" /><p>Loading Comments</p></div> : commentData.map((comment) => {
+                    return <ul key={comment.comment_id} id="comment-box">
+                        <h3 id="comment-author"></h3>
+                        <p>{comment.body}</p>
+                        <p>Posted at:{comment.created_at}</p>
+                        <p>Votes: {comment.votes } 👍</p> <a> Like Comment</a>
+                    </ul>
+                })
+                }
+            </section>
             <div id="controls" className="topnav">
                 <a href="/">Back to All Articles</a>
                 <a herf="/myaccount/">Like 👍</a>
                 <a href="#">Add Comment</a>
-                
             </div>
             
         </ul>
